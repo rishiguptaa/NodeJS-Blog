fadcaad75eb7e39d9499893311acc46c3b8f45b5
rishiguptaa <49725966+rishiguptaa@users.noreply.github.com>
2020-11-02 13:42:35 +0530

    Create index.js

---
+++ views/articles/index.js
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SHARE</title>
+  <style>
+  a{
+    color: black;
+    text-decoration: none;
+  }
+  a:hover{
+    cursor: pointer;
+    text-decoration: none;
+    color: black;
+  }
+  #grad {
+  background-image: linear-gradient(to right, rgba(12,236,199,0.1), rgba(12,236,199,0.5), rgb(12, 236, 199,1));
+  }
+  #position{
+    color: black;
+    border-radius: 30%;
+    height: 40px;
+    width: 40px;
+    position: absolute;
+    /* left: 1190px; */
+    right: 125px;
+    top: 10px;
+  }
+  .hide {
+    display: none;
+    font-size: small;
+  }
+.btn:hover + .hide {
+  display: block;
+  color:black;
+  position: absolute;
+  right: 180px;
+  top: 18px;
+  background-color: gray;
+  border-radius: 10px;
+  padding: 5px;
+  }
+  </style>
+</head>
+<body id="grad">
+  <div class="container mt-10">
+    <a href="/"><h1 class="mb-4 mt-6">SHARE
+    <a href="/articles/new" class="btn btn-success" id="position">+</a>
+    <p class="hide">New Article</p></h1></a>
+
+    <% articles.forEach(article => { %>
+      <div class="card mt-4">
+        <div class="card-body">
+          <h4 class="card-title"><%= article.title %></h4>
+          <div class="card-subtitle text-muted mb-2">
+            <%= article.createdAt.toLocaleDateString() %>
+          </div>
+          <div class="card-text mb-2"><%= article.description %></div>
+          <a href="articles/<%= article.slug %>" class="btn btn-primary">Read More</a>
+          <a href="articles/edit/<%= article.id %>" class="btn btn-info">Edit</a>
+          <form action="/articles/<%= article.id %>?_method=DELETE" method="POST" class="d-inline">
+            <button type="submit" class="btn btn-danger">Delete</button>
+          </form>
+        </div>
+      </div>
+    <% }) %>
+  </div>
+</body>
+</html>
